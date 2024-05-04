a77bc3e214be3578d0b5b1dcf6d2ed2663a7e4ae
Nguyễn Minh Nhật <116283788+nmnhatUIT03@users.noreply.github.com>
2024-05-04 14:10:40 +0700

    Adding DTO files

---
+++ DTO/DTO_Kho.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class DTO_Kho
+    {
+        public string sMaPhuTung;
+        public string sTenVatTuPhuTung;
+        public int iSoLuong;
+        public int iDonGia;
+
+        public string MaPhuTung
+        {
+            get { return MaPhuTung; }
+            set { MaPhuTung = value; }
+        }
+
+        public string TenVatTuPhuTung
+        {
+            get { return TenVatTuPhuTung; }
+            set { TenVatTuPhuTung = value; }
+        }
+
+        public int SoLuong
+        {
+            get { return SoLuong; }
+            set { SoLuong = value; }
+        }
+
+        public int DonGia
+        {
+            get { return DonGia; }
+            set { DonGia = value; }
+        }
+
+    }
+}
---
+++ DTO/DTO_Xe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DTO_Xe
+    {
+        public int iMaKH;
+        public string sBienSo;
+        public string sHieuXe;
+        public DateTime dtNgaySuaChua;
+
+        public int MaKH
+        {
+            get { return iMaKH; }
+            set { iMaKH = value; }
+        }
+
+        public string BienSo
+        {
+            get { return sBienSo; }
+            set { sBienSo = value; }
+        }
+
+        public string HieuXe
+        {
+            get { return sHieuXe; }
+            set { sHieuXe = value; }
+        }
+
+        public DateTime NgaySuaChua
+        {
+            get { return dtNgaySuaChua; }
+            set { dtNgaySuaChua = value; }
+        }
+
+        public DTO_Xe(int makh,string bienso, string hieuxe, DateTime ngaysuachua)
+        {
+            this.iMaKH = makh;
+            this.sBienSo = bienso;
+            this.sHieuXe = hieuxe;
+            this.dtNgaySuaChua = ngaysuachua;
+        }
+        public DTO_Xe()
+        {
+
+        }
+    }
+}
